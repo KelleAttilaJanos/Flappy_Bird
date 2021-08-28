914d59435ba5a13be6d3f686bc63942e2e76099c
Kelle Attila <kelle.attila@simonyiszki.org>
2021-08-28 09:42:56 +0200

    Add project files.

---
+++ Flappy Bird/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Flappy_Bird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 716);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1896, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(180, 200);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(0, 974);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Start";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(1630, 965);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(254, 42);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "MaxRecord:0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1896, 1016);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Flappy Bird";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Flappy Bird/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy_Bird
+{
+    public partial class Form1 : Form
+    {
+        int position = 200;
+        int down = 4;
+        int pipespeed = -1;
+        int score = 0;
+        public Form1()
+        {
+            hatter a = new hatter();
+            InitializeComponent();
+            PictureBox[,] oszlopok = new PictureBox[2, 7];
+            int[] oposition = new int[7];
+            int[] randomsz = new int[7];
+            a.maxrecord=a.Import();
+            label2.Text = "Record: " + a.maxrecord;
+            Random szam = new Random();
+            for (int i = 0; i < 7; i++)
+            {
+                int rszam = szam.Next(180, 400);
+                oposition[i] = 1000+i*300;
+                oszlopok[0, i] = new PictureBox();
+                oszlopok[1, i] = new PictureBox();
+                oszlopok[0, i].BackColor = Color.White;
+                oszlopok[1, i].BackColor = Color.White;
+                randomsz[i] = rszam;
+                oszlopok[1, i].Location = new Point(oposition[i], randomsz[i]);
+                oszlopok[0, i].Location = new Point(oposition[i], 0);
+                oszlopok[0, i].Size = new Size(60, randomsz[i] - 130);
+                oszlopok[1, i].Size = new Size(60, 1000 - randomsz[i]);
+                this.Controls.Add(oszlopok[1, i]);
+                this.Controls.Add(oszlopok[0, i]);
+            }
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddEllipse(new Rectangle(0, 0, pictureBox2.Width - 1, pictureBox2.Height - 1));
+            pictureBox2.Region = new Region(gp);
+            pictureBox2.Location = new Point(50, position);
+            KeyDown += (s, e) => { timer1.Start(); label2.Text ="0 pont"; };
+            int time = 0;
+            int tav = 0;
+            timer1.Tick += (s, e) =>
+            {
+                time++;
+                if(pipespeed<10)
+                {
+                    pipespeed = -1-Convert.ToInt32(time / 1000);
+                }
+                else
+                {
+                    pipespeed = 10;
+                }
+                if(position<0)
+                {
+                    down = 10;
+                }
+                tav += pipespeed * -1;
+                position += down;
+                pictureBox2.Location = new Point(50, position);
+                for (int i = 0; i < 7; i++)
+                {
+                    oposition[i] += pipespeed;
+                    if(oposition[i]<-1100)
+                    {
+                        oposition[i] = 1000;
+                        randomsz[i] = szam.Next(180, 400);
+                    }
+                    oszlopok[0, i].Location = new Point(oposition[i], 0);
+                    oszlopok[1, i].Location = new Point(oposition[i], randomsz[i]);
+                    oszlopok[0, i].Size = new Size(60, randomsz[i] - 130);
+                    oszlopok[1, i].Size = new Size(60, 1000 - randomsz[i]);
+                }
+                score = Convert.ToInt32((tav - 700) / 300);
+                label1.Text = Convert.ToString(Convert.ToInt32(time / 70 / 60)) + ":" + Convert.ToString(Convert.ToInt32(time / 70 % 60));
+                if (score > 0) { label2.Text = score + " pont";}
+                for (int i = 0; i < 7; i++)
+                {
+                    if (oszlopok[0, i].Bounds.IntersectsWith(pictureBox2.Bounds) || oszlopok[1, i].Bounds.IntersectsWith(pictureBox2.Bounds))
+                    {
+                        timer1.Stop();
+                        label1.Text = ("Game Over");
+                        if (a.Export(score))
+                        {
+                            label2.Text = "New Record";
+                        }
+                        else
+                        {
+                            label2.Text = "Try Again";
+                        }
+                    }
+                }
+                if (pictureBox2.Bounds.IntersectsWith(pictureBox1.Bounds))
+                {
+                    timer1.Stop();
+                    label1.Text = ("Game Over");
+                    if (a.Export(score))
+                    {
+                        label2.Text = "New Record";
+                    }
+                    else
+                    {
+                        label2.Text = "Try Again";
+                    }
+                }
+            };
+            KeyDown += (s, e) => { down = -4; }; 
+            KeyUp += (s, e) => { down = 3; };
+            KeyDown += (s, e) => { if (label1.Text == "Game Over") { Application.Restart(); } };
+        }
+    }
+}
